ebe8616b3b275e4dad1bbfe73cb398ca25df4c3a
andrii.sultanovskyi <andrii.sultanovskyi@gmail.com>
2025-06-20 18:16:22 +0200

    Adding dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,12 +30,13 @@ def create_driver():
     options.add_argument("--disable-popup-blocking")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless")
-    # options.add_argument("--headless=new")
+    # options.add_argument("--headless")
+    options.add_argument("--headless=new")
     # options.add_argument("--headless=old")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # driver = webdriver.Chrome(options=options)
+    options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+    options=options)
     return driver
 
 
