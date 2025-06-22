97640b1b628adeb1191a54e49d8b3b5a9b7d526f
andrii.sultanovskyi <andrii.sultanovskyi@gmail.com>
2025-06-22 20:01:57 +0200

    adding class HomePage

--- tests/tests_home_page.py
+++ tests/tests_home_page.py
@@ -3,13 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
-import time
+from pages.homepage import HomePage
 
 
 
 
 # The test checks the click of the "Test" button on the header and goes to the 'Test for Students' page
 def test_click_button_news(driver, close_cookie_popup):
+    homepage = HomePage(driver)
+    homepage.open()
     element = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.LINK_TEXT, "News"))
     )
